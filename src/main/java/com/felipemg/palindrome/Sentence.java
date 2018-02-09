d3efc62edae8dc56a3da6845e5854e9bfa706ecf
FelipeMG <felipemontana@gmail.com>
2018-02-08 19:22:26 -0500

    added tests

--- src/main/java/com/felipemg/palindrome/Sentence.java
+++ src/main/java/com/felipemg/palindrome/Sentence.java
@@ -7,7 +7,36 @@ public final class Sentence{
     }
 
     public static boolean isPalindrome(String sentence){
-        return true;
+        String readableSentence = removeSpecialCharacters(sentence);
+        String readableLowerCaseSentence = readableSentence.toLowerCase();
+        return isPalindromeStringWithoutSpecialCharacters(readableLowerCaseSentence);
     }
 
+    private static String removeSpecialCharacters(String sentence) {
+        return sentence.replaceAll("[ !¡,]", "");
+    }
+
+    private static boolean isPalindromeStringWithoutSpecialCharacters(String sentence) {
+        if(sentence.isEmpty() || isAOneCharacterSentence(sentence)){
+            return true;
+        }
+        else if ( areFirstAndLastCharacterEqual(sentence) ){
+            return isPalindromeStringWithoutSpecialCharacters(substringWithoutFirstAndLastCharacter(sentence));
+        }
+        else{
+            return false;
+        }
+    }
+
+    private static boolean isAOneCharacterSentence(String sentence) {
+        return sentence.length() == 1;
+    }
+
+    private static boolean areFirstAndLastCharacterEqual(String sentence) {
+        return sentence.charAt(0) == sentence.charAt(sentence.length()-1);
+    }
+
+    private static String substringWithoutFirstAndLastCharacter(String sentence) {
+        return sentence.substring(1,sentence.length()-1);
+    }
 }
--- src/test/java/com/felipemg/palindrome/PalindromeTest.java
+++ src/test/java/com/felipemg/palindrome/PalindromeTest.java
@@ -12,5 +12,49 @@ public class PalindromeTest {
         assertThat(Sentence.isPalindrome(""),is(true));
     }
 
+    @Test
+    public void oneCharacterStringIsPalindrome(){
+        assertThat(Sentence.isPalindrome("a"),is(true));
+    }
+
+    @Test
+    public void abIsNotPalindrome(){
+        assertThat(Sentence.isPalindrome("ab"),is(false));
+    }
+
+    @Test
+    public void abaIsPalindrome(){
+        assertThat(Sentence.isPalindrome("aba"),is(true));
+    }
+
+    @Test
+    public void abbaIsPalindrome(){
+        assertThat(Sentence.isPalindrome("abba"),is(true));
+    }
+
+    @Test
+    public void readerIsNotPalindrome(){
+        assertThat(Sentence.isPalindrome("reader"),is(false));
+    }
+
+    @Test
+    public void radarIsPalindrome(){
+        assertThat(Sentence.isPalindrome("radar"),is(true));
+    }
+
+    @Test
+    public void aStringWithSpacesCanAlsoBePalindrome(){
+        assertThat(Sentence.isPalindrome("anita lava la tina"),is(true));
+    }
+
+    @Test
+    public void aStringWithSpacesAndSpecialCharactersCanAlsoBePalindrome(){
+        assertThat(Sentence.isPalindrome("¡anita, lava, la tina!"),is(true));
+    }
+
+    @Test
+    public void aStringWithCapitalLettersCanAlsoBePalindrome(){
+        assertThat(Sentence.isPalindrome("¡anita, lava, la Tina!"),is(true));
+    }
 
 }
